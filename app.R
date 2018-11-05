2991fd42fd6128b80e2f9c532ab04845b068790d
Brett Kobold <brettkobold@BrettKobold.local>
2018-11-05 18:36:21 -0500

    Initial commit.

---
+++ app.R
@@ -0,0 +1,174 @@
+library(shiny)
+
+# which fields get saved 
+fieldsAll <- c("emotion", "thoughts", "rating")
+
+# which fields are mandatory
+fieldsMandatory <- c("emotion", "thoughts")
+
+# add an asterisk to an input label
+labelMandatory <- function(label) {
+  tagList(
+    label,
+    span("*", class = "mandatory_star")
+  )
+}
+
+# get current Epoch time
+epochTime <- function() {
+  return(as.integer(Sys.time()))
+}
+
+# get a formatted string of the timestamp (exclude colons as they are invalid
+# characters in Windows filenames)
+humanTime <- function() {
+  format(Sys.time(), "%Y%m%d-%H%M%OS")
+}
+
+# save the results to a file
+saveData <- function(data) {
+  fileName <- sprintf("%s_%s.csv",
+                      humanTime(),
+                      digest::digest(data))
+  
+  write.csv(x = data, file = file.path(responsesDir, fileName),
+            row.names = FALSE, quote = TRUE)
+}
+
+# load all responses into a data.frame
+loadData <- function() {
+  files <- list.files(path = responsesDir, full.names = TRUE)
+  data <- lapply(files, read.csv, stringsAsFactors = FALSE)
+  data <- do.call(rbind, data)
+  data
+}
+
+# directory where responses get stored
+responsesDir <- "responses"
+
+# CSS to use in the app
+appCSS <-
+  ".mandatory_star { color: red; }
+   .shiny-input-container { margin-top: 25px; }
+   #submit_msg { margin-left: 15px; }
+   #error { color: red; }
+   body { background: #fcfcfc; }
+   #header { background: #fff; border-bottom: 1px solid #ddd; margin: -20px -15px 0; padding: 15px 15px 10px; }
+  "
+
+# usernames that are admins
+adminUsers <- c("admin", "prof")
+
+# info for sharing this app on facebook/twitter
+
+shinyApp(
+  ui = fluidPage(
+    shinyjs::useShinyjs(),
+    shinyjs::inlineCSS(appCSS),
+    titlePanel("Emotion Tracker v.1"),
+    sidebarLayout(
+      sidebarPanel(
+               id = "form",
+               textInput("emotion", labelMandatory("Emotion"), ""),
+               textInput("thoughts", labelMandatory("Thoughts"), ""),
+               sliderInput("rating", "Rating", 0, 10, 5, ticks = FALSE),
+               actionButton("submit", "Submit", class = "btn-primary")
+        ),
+      mainPanel(
+             uiOutput("adminPanelContainer")
+      )
+    )
+  ),
+  
+  server = function(input, output, session) {
+    
+    # Enable the Submit button when all mandatory fields are filled out
+    observe({
+      mandatoryFilled <-
+        vapply(fieldsMandatory,
+               function(x) {
+                 !is.null(input[[x]]) && input[[x]] != ""
+               },
+               logical(1))
+      mandatoryFilled <- all(mandatoryFilled)
+      
+      shinyjs::toggleState(id = "submit", condition = mandatoryFilled)
+    })
+    
+    # Gather all the form inputs (and add timestamp)
+    formData <- reactive({
+      data <- sapply(fieldsAll, function(x) input[[x]])
+      data <- c(data, timestamp = epochTime())
+      data <- t(data)
+      data
+    })    
+    
+    # When the Submit button is clicked, submit the response
+    observeEvent(input$submit, {
+      
+      # User-experience stuff
+      shinyjs::disable("submit")
+      shinyjs::show("submit_msg")
+      shinyjs::hide("error")
+      
+      # Save the data (show an error message in case of error)
+      tryCatch({
+        saveData(formData())
+        shinyjs::reset("form")
+      },
+      error = function(err) {
+        shinyjs::html("error_msg", err$message)
+        shinyjs::show(id = "error", anim = TRUE, animType = "fade")
+      },
+      finally = {
+        shinyjs::enable("submit")
+        shinyjs::hide("submit_msg")
+      })
+    })
+    
+    # submit another response
+    observeEvent(input$submit_another, {
+      shinyjs::show("form")
+      shinyjs::hide("thankyou_msg")
+    })
+    
+    # render the admin panel
+    output$adminPanelContainer <- renderUI({
+      if (!isAdmin()) return()
+      
+      div(
+        id = "adminPanel",
+        h2("Previous responses (only visible to admins)"),
+        downloadButton("downloadBtn", "Download responses"), br(), br(),
+        DT::dataTableOutput("responsesTable")
+      )
+    })
+    
+    # determine if current user is admin
+    isAdmin <- reactive({
+      is.null(session$user) || session$user %in% adminUsers
+    })    
+    
+    # Show the responses in the admin table
+    output$responsesTable <- DT::renderDataTable({
+      input$submit
+      data <- loadData()
+      data$timestamp <- as.POSIXct(data$timestamp, origin="1970-01-01")
+      DT::datatable(
+        data,
+        rownames = FALSE,
+        options = list(searching = FALSE, lengthChange = FALSE)
+      )
+    })
+    
+    # Allow user to download responses
+    output$downloadBtn <- downloadHandler(
+      filename = function() { 
+        sprintf("mimic-google-form_%s.csv", humanTime())
+      },
+      content = function(file) {
+        write.csv(loadData(), file, row.names = FALSE)
+      }
+    )    
+  }
+)
